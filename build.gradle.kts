2d1d8d605c9f3e1a25419974b0c2da4213c318aa
Antonio Fernández Alabarce <afalabarce@gmail.com>
2023-05-13 14:53:32 +0200

    First Library Commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript{
+    dependencies{
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+plugins {
+    id("com.android.application") version "8.2.0-alpha03" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.library") version "8.2.0-alpha03" apply false
+}
---
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -0,0 +1,34 @@
+object Constants {
+    const val versionCode = 153
+    const val versionName = "1.5.3"
+    const val composeVersion = "1.5.0-alpha04"
+    const val composeCompilerVersion = "1.4.4"
+    const val androidxCoreKtxVersion = "1.10.1"
+    const val androidxAppCompatVersion = "1.6.1"
+    const val composeMaterial3Version = "1.1.0"
+    const val firebaseBomVersion = "30.3.1"
+    const val firebaseAppCheckDebugVersion = "17.0.0"
+    const val firebaseMessagingVersion = "23.1.2"
+    const val gmsPlayServicesAdsVersion = "22.0.0"
+    const val gmsPlayServicesAdsVersionIdentifierVersion = "18.0.1"
+    const val lifeCycleRuntimeVersion = "2.6.1"
+    const val activityComposeVersion = "1.7.1"
+    const val constraintLayoutComposeVersion = "1.0.1"
+}
+
+object SonatypePublishing {
+    const val publishedGroupId = "io.github.afalabarce"
+    const val libraryName = "jetpackcompose-admob"
+    const val artifact = "jetpackcompose-admob"
+    const val libraryDescription = "Another Project for Jetpack Compose Composable Library (for admob publishers)"
+    const val siteUrl = "https://github.com/afalabarce/jetpackcompose-admob"
+    const val gitUrl = "https://github.com/afalabarce/jetpackcompose-admob.git"
+    const val libraryVersionId = Constants.versionCode
+    const val libraryVersionCode = Constants.versionName
+    const val developerId = "afalabarce"
+    const val developerName = "Antonio Fdez. Alabarce"
+    const val developerEmail = "afalabarce@gmail.com"
+    const val licenseName = "The Apache Software License, Version 2.0"
+    const val licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+    val allLicenses = arrayOf ("Apache-2.0")
+}
